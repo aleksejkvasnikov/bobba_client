4d8ca557c30ed334ca24c14d7398afa283c2e8ba
Jevgeņijs Ļežņins <z_lezhnin@mail2000.ru>
2023-10-01 02:44:57 +0300

    add text edit for motto

--- src/ui/iteminfo/MottoEdit.jsx
+++ src/ui/iteminfo/MottoEdit.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import 'react-edit-text/dist/index.css';
 
 export default class MottoEdit extends React.Component {
     render() {
         const { motto, onMottoChange } = this.props;
-        return '';
+        return <input type="text" ref={motto} maxLength={80} name="chat" value={motto} autoComplete="off" onChange={event => onMottoChange(event.target.value)} />;
     }
 }
 
